8dd4c65461892ad906c55f62197743f721db2c0d
Max Avramenko <max.avramenko@jpmorgan.com>
2015-03-25 14:39:41 -0400

    Test commit

---
+++ helloworld/src/main/java/max/test/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package max.test.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
